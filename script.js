4d3d618732422ce5eb37f3d810a0d2de35194a2a
Theodoric-Graham <theodoricgraham@hotmail.com>
2022-03-02 14:01:10 -0600

    the module pattern

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 //Importing Module
 //preferred style one default export per module, but its not a rigid rule
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
@@ -48,3 +49,44 @@ console.log(getLastPost());
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+*/
+//the main goal of the module pattern is to encapsulate functionality to have
+//private data and to expose a public api
+
+//the best way of this is using a function, they give us private data by default
+//and allow us to return values, which can become our public api
+
+//IIFE we dont have to call it seperately and we can insure its only called once
+// to create a new scope and return data just once
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} was added to the cart(shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+//closures allow a function to have access to all the variables that were
+//present at its birthplace
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
